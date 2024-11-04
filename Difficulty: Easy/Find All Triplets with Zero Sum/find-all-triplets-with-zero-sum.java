93e35d89949eeac7b44781c50ccd8599010dabbb
Chirag Yadav <72174095+chiragyadav916@users.noreply.github.com>
2024-11-04 08:17:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+
+    public List<List<Integer>> findTriplets(int[] arr) {
+
+        // Your code here
+
+        List<List<Integer>>li=new ArrayList<>();
+
+        List<Integer>temp=new ArrayList<>();
+
+        int k=0;
+
+        int j=0;
+
+        int size=0;
+
+        int i=0;
+
+        boolean isin=false;
+
+        for( i=0;i<arr.length;i++){
+
+            for(j=i+1;j<arr.length;j++){
+
+                for(k=j+1;k<arr.length;k++){
+
+                  if(arr[i]+arr[j]+arr[k]==0){
+
+                            temp.add(i);
+
+                           temp.add(j);
+
+                           temp.add(k);
+
+                          li.add(size,temp);
+
+                         size++;
+
+                        temp=new ArrayList<>();
+
+                 }
+
+                }    
+
+                }          
+
+            }
+
+        return li;
+
+        }
+}
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            List<List<Integer>> res = obj.findTriplets(nums);
+            Collections.sort(res, (a, b) -> {
+                for (int i = 0; i < a.size(); i++) {
+                    if (!a.get(i).equals(b.get(i))) {
+                        return a.get(i) - b.get(i);
+                    }
+                }
+                return 0;
+            });
+            if (res.size() == 0) {
+                System.out.println("[]");
+            }
+            for (int i = 0; i < res.size(); i++) {
+                for (int j = 0; j < res.get(i).size(); j++) {
+                    System.out.print(res.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
